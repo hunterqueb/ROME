ffabc66c25e8977452c7effe496606ade8ab08cb
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-05-31 19:13:14 -0400

    added get OrientErr Function

--- ROME-Python/libraries/arm functions/orientationFunctions.py
+++ ROME-Python/libraries/arm functions/orientationFunctions.py
@@ -7,10 +7,34 @@ import numpy as np
 
 def Vec2Skew(v):
     # Function to generate the skew symmetric matrix corresponding to a vector
-    Mx = np.asmatrix([0, -v[2], v[1]],[v[2], 0, -v[0]],[-v[1], v[0], 0])
+    Mx = np.asmatrix([[0, -v[2], v[1]],[v[2], 0, -v[0]],[-v[1], v[0], 0]])
 
     return Mx
 
 def getOrientErr(C_ref,C):
-    pass
+    
+    # [:,1] gets row vector
+    # [1,:] gets column vector
+    nd = C_ref[:, 0]
+    sd = C_ref[:, 1]
+    ad = C_ref[:, 2]
+    
+    ne = C[:, 0]
+    se = C[:, 1]
+    ae = C[:, 2]
+    
+    S_nd = Vec2Skew(nd)
+    S_sd = Vec2Skew(sd)
+    S_ad = Vec2Skew(ad)
+    S_ne = Vec2Skew(ne)
+    S_se = Vec2Skew(se)
+    S_ae = Vec2Skew(ae)
+
+
+    eo = 0.5*(np.cross(ne, nd) + np.cross(se, sd) + np.cross(ae, ad))
+    L = -0.5*(S_nd*S_ne + S_sd*S_se + S_ad*S_ae)
+
+    return [eo, L]
+
+
 
