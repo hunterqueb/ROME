89e48c18860b9c558c2f40cfb47dc9666bbfd4c1
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-09-23 15:37:59 -0400

    changed input format to match ROMEFK

--- libraries/ROME/Kinematics/J_ROME.m
+++ libraries/ROME/Kinematics/J_ROME.m
@@ -5,45 +5,45 @@ d3 = 0;         a3 = 0;     alpha3 = 90*pi/180;
 d4 = -222.63;   a4 = 0;     alpha4 = -90*pi/180;
 d5 = 0;         a5 = 0;     alpha5 = 90*pi/180;
 d6 = -36.25;    a6 = 0;     alpha6 = 0;
-theta1 = theta0(1);
+theta1 = theta0(4);
 cth1 = cos(theta1);
 sal1 = sin(alpha1);
 sth1 = sin(theta1);
 cal1 = cos(alpha1);
 
-theta2 = theta0(2);
+theta2 = theta0(5);
 cth2 = cos(theta2);
 sal2 = sin(alpha2);
 sth2 = sin(theta2);
 cal2 = cos(alpha2);
 
-theta3 = theta0(3);
+theta3 = theta0(6);
 cth3 = cos(theta3);
 sal3 = sin(alpha3);
 sth3 = sin(theta3);
 cal3 = cos(alpha3);
 
-theta4 = theta0(4);
+theta4 = theta0(7);
 cth4 = cos(theta4);
 sal4 = sin(alpha4);
 sth4 = sin(theta4);
 cal4 = cos(alpha4);
 
-theta5 = theta0(5);
+theta5 = theta0(8);
 cth5 = cos(theta5);
 sal5 = sin(alpha5);
 sth5 = sin(theta5);
 cal5 = cos(alpha5);
 
-theta6 = theta0(6);
+theta6 = theta0(9);
 cth6 = cos(theta6);
 sal6 = sin(alpha6);
 sth6 = sin(theta6);
 cal6 = cos(alpha6);
 
-pc6_1 = theta0(7);
-pc6_2 = theta0(8);
-psi = theta0(9);
+pc6_1 = theta0(1);
+pc6_2 = theta0(2);
+psi = theta0(3);
 
 psiSin = sin(psi);
 psiCos = cos(psi);
