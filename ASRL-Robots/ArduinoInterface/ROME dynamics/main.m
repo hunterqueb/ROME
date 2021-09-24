9dff2751733004ce9ba35890a5b1c4859248c55d
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-09-24 10:47:29 -0400

    added figure to show desired motion and added all boilerplate before sim loop

--- ASRL-Robots/ArduinoInterface/ROME dynamics/main.m
+++ ASRL-Robots/ArduinoInterface/ROME dynamics/main.m
@@ -1,29 +1,50 @@
-
-tSim = 30; % in sec
-
+% initialize the time vector
+tSim = 30; % in sec - time for total sim time
 h = 0.01;
-
 t = (h:h:tSim)';
 
-% initial arm pos in joint
+% initial arm pos in joint - rest
 theta0Arm = [0,-110*pi/180,141*pi/180,0,0,0];
+% inital gv pos in joint - at zero zero
+gvInitx = 0; % in mm
+gvInity = 0;
+gvYaw = 0;
+theta0GV = [gvInitx,gvInity,gvYaw];
 
-% inital gv pos in joint
-theta0GV = [0,0,0];
-
+% inital vehicle pos
 theta0 = [theta0GV,theta0Arm];
 
 % initial arm pos in task space
 [state0,ori0] = ROMEFK(theta0);
 
-
+% constants to define the scale of sinisoid
 radOfCircle = 1000; %in mm
 verticalReach = 80;
-for i = l:length(t)
-    x = [radOfCircle*cos(2*pi*t(i)/tSim) radOfCircle*sin(2*pi*t(i)/tSim verticalReach*sin(2*pi*t(i)/tSim) 0 0 0] + [state0,ori0];
-    xdot = [];
-    xddot = [];
+
+% define the vectors for the x,xdot,and xddot for the desired motion
+x_des = zeros(length(t),6);
+xdot_des = zeros(length(t),6);
+xddot_des = zeros(length(t),6);
+
+x_sim = x_des;
+xdot_sim = xdot_des;
+xddot_sim = xddot_des;
+
+
+for i = 1:length(t)
+    x_des(i,:) = [radOfCircle*cos(2*pi*t(i)/tSim) radOfCircle*sin(2*pi*t(i)/tSim) verticalReach*sin(2*pi*t(i)/tSim) 0 0 0] + [state0',ori0'];
+    xdot_des(i,:) = [radOfCircle*-sin(2*pi*t(i)/tSim)*2*pi/tSim radOfCircle*cos(2*pi*t(i)/tSim)*2*pi/tSim verticalReach*cos(2*pi*t(i)/tSim)*2*pi/tSim 0 0 0];
+    xddot_des(i,:) = [radOfCircle*-cos(2*pi*t(i)/tSim)*(2*pi/tSim)^2 radOfCircle*-sin(2*pi*t(i)/tSim)*(2*pi/tSim)^2 verticalReach*cos(2*pi*t(i)/tSim)*(2*pi/tSim)^2 0 0 0];
 end
 
+figure
+plot3(x_des(:,1),x_des(:,2),x_des(:,3))
+grid on
+title("Desired Trajectory")
+
+for i = 1:length(t)
+%     main script to calc forces
+    
+end
 
 
