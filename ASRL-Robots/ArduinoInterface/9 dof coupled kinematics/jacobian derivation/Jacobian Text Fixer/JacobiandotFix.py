0d2c81ebc9a4356ea7fde83c92b6d4b0152fdb1c
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-10-05 01:45:18 -0400

    fixed jdot fixer

--- ASRL-Robots/ArduinoInterface/9 dof coupled kinematics/jacobian derivation/Jacobian Text Fixer/JacobiandotFix.py
+++ ASRL-Robots/ArduinoInterface/9 dof coupled kinematics/jacobian derivation/Jacobian Text Fixer/JacobiandotFix.py
@@ -29,16 +29,18 @@ content = content.replace("matrix(","")
 content = rreplace(content,')',';\n',1)
 # remember to remove to last close parenthesis on the matrix
 
-Jac = "function J = J_ROME(theta0)\n"
+Jac = "function J = Jdot_ROME(theta0,thetadot0)\n"
 
 Jac = Jac + "d1 = 169.77;    a1 = 64.2;  alpha1 = -90*pi/180;\nd2 = 0;         a2 = 305;   alpha2 = 0;\nd3 = 0;         a3 = 0;     alpha3 = 90*pi/180;\nd4 = -222.63;   a4 = 0;     alpha4 = -90*pi/180;\nd5 = 0;         a5 = 0;     alpha5 = 90*pi/180;\nd6 = -36.25;    a6 = 0;     alpha6 = 0;\n"
 
 for num in range(6):
-    Jac = Jac + "theta"+str(num+1)+" = theta0("+str(num+1+3)+");\n"
+    Jac = Jac + "theta"+str(num+1)+" = theta0("+str(num+1)+");\n"
+    Jac = Jac + "thetadot"+str(num+1)+" = thetadot0("+str(num+1)+");\n"
     Jac = Jac + "cth"+str(num+1)+ " = cos(theta"+str(num+1)+");\n"
     Jac = Jac + "sal"+str(num+1)+ " = sin(alpha"+str(num+1)+");\n"
     Jac = Jac + "sth"+str(num+1)+ " = sin(theta"+str(num+1)+");\n"
     Jac = Jac + "cal"+str(num+1)+ " = cos(alpha"+str(num+1)+");\n\n"
+Jac = Jac + "psidot"+str(num+1)+" = thetadot0("+str(8+1)+");\n"
 Jac = Jac + "pc6_1 = theta0(7);\n"
 Jac = Jac + "pc6_2 = theta0(8);\n"
 Jac = Jac + "psi = theta0(9);\n"
@@ -47,7 +49,7 @@ Jac = Jac + "psiCos = cos(psi);\n\n"
 Jac = Jac + "J = " + content
 Jac = Jac + "end"
 
-f = open('J_ROME.m', 'w')
+f = open('Jdot_ROME.m', 'w')
 
 f.write(Jac)
 
