c7380f656f32d144201495cca302cb02726a9527
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2022-03-02 16:22:28 -0500

    updated openloop kinetmatics function

--- ASRL-Robots/ArduinoInterface/Trajectory_Tracking_Arm/trajectoryTrackingArmNewMotorInterface.m
+++ ASRL-Robots/ArduinoInterface/Trajectory_Tracking_Arm/trajectoryTrackingArmNewMotorInterface.m
@@ -1,4 +1,4 @@
-function [path,pathSteppers] = trajectoryTrackingArmNewMotorInterface(path)
+function trajectoryTrackingArmNewMotorInterface(path,AR3Serial)
 % This function takes a path, specified as 6 angle states followed by six
 % angular velocity states (rad,rad/s) for each joint.
 % Stepper1 is a stepper motor object for one of
@@ -15,7 +15,7 @@ pause(5);
 statesArray = [path(1,2),path(1,3),path(1,4)...
                path(1,5),path(1,6),path(1,7)...
                .25,.25,.25,.25,.25,.25];
-Stepper1.updateStates(statesArray);
+AR3Serial.updateStates(statesArray);
 pause(15);
 
 % Main Loop
@@ -29,7 +29,7 @@ while(toc <= path(end,1))
                        path(index,5),path(index,6),path(index,7)...
                        path(index,8),path(index,9),path(index,10)...
                        path(index,11),path(index,12),path(index,13)];
-        AR3.updateStates(statesArray);
+        AR3Serial.updateStates(statesArray);
         
         index = index + 1;
     end
